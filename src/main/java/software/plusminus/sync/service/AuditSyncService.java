c878dbb523542414392b62fd402ea0602ddbe682
Taras Shpek <taras.shpek@gmail.com>
2024-01-22 22:12:21 +0200

    remove cache functionality

--- src/main/java/software/plusminus/sync/service/AuditSyncService.java
+++ src/main/java/software/plusminus/sync/service/AuditSyncService.java
@@ -1,9 +1,5 @@
 package software.plusminus.sync.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.github.benmanes.caffeine.cache.Cache;
-import com.github.benmanes.caffeine.cache.Caffeine;
 import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
@@ -12,12 +8,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import software.plusminus.audit.annotation.Auditable;
 import software.plusminus.audit.model.AuditLog;
-import software.plusminus.audit.model.DataAction;
 import software.plusminus.audit.repository.AuditLogRepository;
 import software.plusminus.audit.service.AuditLogService;
 import software.plusminus.audit.service.DeviceContext;
@@ -29,10 +23,8 @@ import software.plusminus.sync.dto.Deleted;
 import software.plusminus.sync.dto.Sync;
 import software.plusminus.sync.dto.SyncType;
 import software.plusminus.sync.exception.SyncException;
-import software.plusminus.sync.service.fetcher.SyncTransactionService;
 import software.plusminus.sync.service.listener.SyncListener;
 import software.plusminus.sync.service.listener.SyncPostListener;
-import software.plusminus.tenant.context.TenantContext;
 
 import java.lang.annotation.Annotation;
 import java.util.ArrayList;
@@ -41,8 +33,6 @@ import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 import javax.persistence.Entity;
-
-import static javax.management.timer.Timer.ONE_MINUTE;
 
 @Service
 @ConditionalOnBean(AuditLogService.class)
@@ -54,53 +44,13 @@ public class AuditSyncService implements SyncService {
     @Autowired
     private DataService dataService;
     @Autowired
-    private SyncTransactionService transactionService;
-    @Autowired
     private AuditLogRepository auditLogRepository;
     @Autowired
     private DeviceContext deviceContext;
     @Autowired
-    private TenantContext tenantContext;
-    @Autowired
     private List<SyncListener> listeners;
-    @Autowired
-    private ObjectMapper objectMapper;
     @Autowired(required = false)
     private List<SyncPostListener> postListeners = Collections.emptyList();
-    private final Cache<Long, AuditLog<? extends ApiObject>> cache = Caffeine.newBuilder()
-            .maximumSize(1_000)
-            .build();
-
-    @Scheduled(fixedDelay = ONE_MINUTE)
-    public void refresh() {
-        try {
-            tenantContext.disable();
-            transactionService.run(() -> {
-                if (cache.asMap().isEmpty()) {
-                    Page<AuditLog<? extends ApiObject>> page = auditLogRepository.findByCurrentTrue(
-                            PageRequest.of(0, 1_000, Sort.Direction.DESC, "number"));
-                    page.forEach(auditLog -> {
-                        fetchEntity(auditLog);
-                        cache.put(auditLog.getNumber(), auditLog);
-                    });
-                    return;
-                }
-                List<AuditLog<?>> cachedAuditLogs = new ArrayList<>(cache.asMap().values());
-                cachedAuditLogs.forEach(cachedAuditLog -> {
-                    AuditLog<? extends ApiObject> current =
-                            auditLogRepository.findByEntityTypeAndEntityIdAndCurrentTrue(cachedAuditLog.getEntityType(),
-                                    cachedAuditLog.getEntityId());
-                    if (current != null && !Objects.equals(current.getNumber(), cachedAuditLog.getNumber())) {
-                        fetchEntity(current);
-                        cache.invalidate(cachedAuditLog.getNumber());
-                        cache.put(current.getNumber(), current);
-                    }
-                });
-            });
-        } finally {
-            tenantContext.enable();
-        }
-    }
 
     @Override
     public List<Sync<? extends ApiObject>> read(List<String> types, boolean excludeCurrentDevice,
@@ -168,13 +118,6 @@ public class AuditSyncService implements SyncService {
     }
 
     private Sync<? extends ApiObject> toSync(AuditLog<? extends ApiObject> auditLog) {
-        AuditLog<? extends ApiObject> cached = cache.getIfPresent(auditLog.getNumber());
-        if (cached == null) {
-            fetchEntity(auditLog);
-            cache.put(auditLog.getNumber(), auditLog);
-        } else {
-            auditLog = cached;
-        }
         ApiObject object;
         switch (auditLog.getAction()) {
             case CREATE:
@@ -219,19 +162,5 @@ public class AuditSyncService implements SyncService {
             throw new SyncException("Unknown type: " + auditLog.getEntityType(), e);
         }
         return Deleted.of(type.getSimpleName(), auditLog.getEntityId());
-    }
-
-    private <T extends ApiObject> void fetchEntity(AuditLog<T> auditLog) {
-        try {
-            Sync<? extends ApiObject> sync = Sync.of(
-                    auditLog.getAction() != DataAction.DELETE
-                            ? auditLog.getEntity()
-                            : toDeleted(auditLog),
-                    null, null, null);
-            objectMapper.writeValueAsString(sync);
-        } catch (JsonProcessingException e) {
-            throw new SyncException("Exception during cache update of AuditLog #"
-                    + auditLog.getNumber());
-        }
     }
 }
